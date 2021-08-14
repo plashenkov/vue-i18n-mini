4a0ffa796715c7ea6fe67004487dc9e960a83e68
Yuri Plashenkov <plashenkov@gmail.com>
2021-08-14 05:02:02 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,232 @@
+import {resolveComponent, h, ref} from 'vue'
+import template from 'lodash/template'
+import merge from 'lodash/merge'
+import get from 'lodash/get'
+import cookie from 'js-cookie'
+
+function findBestLangs(preferredLangs, supportedLangs) {
+  const result = []
+
+  supportedLangs.forEach((lang, i) => {
+    let l = lang.toLowerCase()
+    let p = l.split('-')[0]
+    let w = 0
+    let j = -1
+
+    preferredLangs.forEach((lang, i) => {
+      let pl = lang.toLowerCase()
+      let pp = pl.split('-')[0]
+      let pw
+
+      if (pl === l) pw = 3
+      else if (pp === l) pw = 2
+      else if (pl === p) pw = 1
+      else return
+
+      if ((w - pw || j - i) < 0) {
+        w = pw
+        j = i
+      }
+    })
+
+    w > 0 && result.push({l: lang, w, j, i})
+  })
+
+  return result
+    .sort((a, b) => (b.w - a.w) || (a.j - b.j) || (a.i - b.i))
+    .map(lang => lang.l)
+}
+
+export function createI18n(options) {
+  options = merge({
+    langData: null,
+    defaultLang: null,
+    fallbackLang: null,
+    preferredLangHandler: null,
+    routerOptions: {
+      prefixParam: 'lang',
+      prefixForDefaultLang: true,
+    },
+    lodashTemplateOptions: {
+      interpolate: /{([\s\S]+?)}/g,
+      escape: /{{([\s\S]+?)}}/g,
+      evaluate: null,
+    }
+  }, options)
+
+  if (!options.langData) {
+    throw new Error('Please define options.langData')
+  }
+
+  if (!options.defaultLang) {
+    throw new Error('Please define options.defaultLang')
+  }
+
+  if (!options.preferredLangHandler) {
+    options.preferredLangHandler = {
+      async set(lang) {
+        cookie.set('lang', lang, {expires: 365})
+      },
+      async get() {
+        const lang = cookie.get('lang')
+        lang && cookie.set('lang', lang, {expires: 365})
+        return lang
+      }
+    }
+  }
+
+  const supportedLangs = Object.keys(options.langData)
+  const langData = {}
+  const langTemplates = {}
+  const lang = ref(null)
+  let initialized
+  let preferredLang
+  let bestLang
+  let setLangId = 0
+
+  async function loadLangData(lang) {
+    if (langData[lang] !== undefined) return
+    if (typeof options.langData[lang] === 'function') {
+      langData[lang] = null
+      const obj = await options.langData[lang]()
+      langData[lang] = obj.__esModule || obj[Symbol.toStringTag] === 'Module' ? obj.default : obj
+    } else {
+      langData[lang] = options.langData[lang]
+    }
+  }
+
+  function t(lang, id, data) {
+    if (!lang || !langData[lang]) return
+
+    if (langTemplates[lang + id]) {
+      return langTemplates[lang + id](data)
+    }
+
+    const el = get(langData[lang], id)
+
+    if (el === undefined && options.fallbackLang && lang !== options.fallbackLang) {
+      return t(options.fallbackLang, id, data)
+    }
+
+    if (typeof el !== 'string' || !data) return el
+
+    const f = langTemplates[lang + id] = template(el, {
+      ...options.lodashTemplateOptions,
+      imports: langData[lang]
+    })
+
+    return f(data)
+  }
+
+  async function savePreferred(lang) {
+    preferredLang = lang
+    return await options.preferredLangHandler.set(lang)
+  }
+
+  async function preferred() {
+    if (preferredLang !== undefined) return preferredLang
+    preferredLang = await options.preferredLangHandler.get() || null
+    return preferredLang
+  }
+
+  function best() {
+    if (bestLang) return bestLang
+    const langs = findBestLangs(navigator.languages || [navigator.language], supportedLangs)
+    bestLang = langs.length ? langs[0] : options.defaultLang
+    return bestLang
+  }
+
+  async function preferredOrBest() {
+    return await preferred() || best()
+  }
+
+  function routePrefix(langs, optional, children) {
+    return {
+      path: `/:${options.routerOptions.prefixParam}(${langs.join('|')})` + (optional ? '?' : ''),
+      children,
+      component: {
+        setup() {
+          const c = resolveComponent('router-view')
+          return () => h(c)
+        }
+      }
+    }
+  }
+
+  return {
+    get initialized() {
+      return !!lang.value
+    },
+
+    get loading() {
+      const langs = Object.entries(langData).filter(l => l[1] === null).map(l => l[0])
+      return langs.length ? false : langs
+    },
+
+    t(id, data = null) {
+      if (lang.value) return t(lang.value, id, data)
+    },
+
+    get lang() {
+      return lang.value
+    },
+
+    async setLang(value, save = false) {
+      const id = ++setLangId
+
+      save && savePreferred(value)
+
+      if (lang.value === value) return
+
+      if (!options.fallbackLang || value === options.fallbackLang) {
+        await loadLangData(value)
+      } else {
+        await Promise.all([loadLangData(options.fallbackLang), loadLangData(value)])
+      }
+
+      if (id === setLangId) {
+        lang.value = value
+      }
+    },
+
+    routePrefix(lang, children) {
+      return routePrefix([lang], lang === options.defaultLang, children)
+    },
+
+    universalRoutePrefix(children) {
+      return routePrefix(supportedLangs, true, children)
+    },
+
+    useRouter(router) {
+      if (initialized) return
+      initialized = true
+
+      const prefixForDefaultLang = options.routerOptions.prefixForDefaultLang
+
+      router.beforeEach(async to => {
+        const langParam = to.params[options.routerOptions.prefixParam]
+
+        if (!prefixForDefaultLang && langParam === options.defaultLang) {
+          return to.fullPath.substring(options.defaultLang.length + 1)
+        }
+
+        if (prefixForDefaultLang && langParam === '') {
+          return '/' + (await preferredOrBest()) + to.fullPath
+        }
+
+        this.setLang(langParam || (langParam === '' ? options.defaultLang : await preferredOrBest()))
+      })
+    },
+
+    async init() {
+      if (initialized) return
+      initialized = true
+      await this.setLang(await preferredOrBest())
+    },
+
+    install(app) {
+      app.config.globalProperties.$i18n = this
+      app.config.globalProperties.$t = this.t.bind(this)
+    }
+  }
+}
